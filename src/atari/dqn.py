624c3cb062756649ae06ef653f7c4bd85e12a47f
Chen Bo Calvin Zhang <chenbocalvinzhang@gmail.com>
2021-02-06 10:11:15 +0000

    DQN architecture

---
+++ src/atari/dqn.py
@@ -0,0 +1,32 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torchvision.transforms as T
+
+class DQN(nn.Module):
+
+    def __init__(self, in_channels=4, num_actions=18):
+        super(DQN, self).__init__()
+
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels, 32, kernel_size=8, stride=4),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1),
+            nn.ReLU()
+        )
+
+        self.fc = nn.Sequential(
+            nn.Linear(7 * 7 * 64, 512),
+            nn.ReLU(),
+            nn.Linear(512, self.num_actions)
+        )
+
+    
+    def forward(self, x):
+        x = self.conv(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return x
