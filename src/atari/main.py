9382de13a5526bacbeb2a0f5d026e56365a964ef
Chen Bo Calvin Zhang <chenbocalvinzhang@gmail.com>
2021-03-11 11:16:23 +0000

    Clean code and add video recording

--- src/atari/main.py
+++ src/atari/main.py
@@ -17,6 +17,7 @@ import torch.optim as optim
 import torch.autograd as autograd
 import torch.nn.functional as F
 
+from gym.wrappers import Monitor
 from atari_wrappers import make_atari, wrap_deepmind
 from replay_buffer import ReplayBuffer
 from dqn import DQN
@@ -33,11 +34,15 @@ def parse_args():
     parser.add_argument('--test', action='store_true')  # False by default, if --test then True
     parser.add_argument('--render', action='store_true')  # False by default, if --render then True
     parser.add_argument('--attack', action='store_true') # False by default, if --attack then True
+    parser.add_argument('game', help='game to train/test on')
 
     return parser.parse_args()
 
 
 def select_action(state, steps_done):
+    '''
+    Select an action given a state using decaying epsilon greedy
+    '''
     sample = random.random()
     eps_threshold = EPS_END + (EPS_START - EPS_END)* \
         math.exp(-1. * steps_done / EPS_DECAY)
@@ -57,13 +62,19 @@ def get_state(obs):
 
 
 def learn():
+    '''
+    This is the optimizer for the training
+    '''
+    # sample from memory
     obses_t, actions, rewards, obses_tp1, dones = buffer.sample(BATCH_SIZE)
     
+    # make a list of tensors adapted to GPU
     actions = [a.to(device) for a in actions]
     rewards = [r.to(device) for r in rewards]
 
+    # mask of non final states
     non_final_mask = torch.tensor([True if o is not None else False for o in obses_tp1], device=device)
-
+    # get non final states
     non_final_next_states = torch.cat([o for o in obses_tp1 if o is not None]).to(device)
 
     state_batch = torch.cat(obses_t).to(device)
@@ -73,14 +84,18 @@ def learn():
     state_action_values = policy_net(state_batch).gather(1, action_batch)
     next_state_values = torch.zeros(BATCH_SIZE, device=device)
     next_state_values[non_final_mask] = target_net(non_final_next_states).max(1)[0].detach()
+    # compute the Q-values
     expected_state_action_values = (next_state_values * GAMMA) + reward_batch
 
+    # compute the loss
     loss = F.smooth_l1_loss(state_action_values, expected_state_action_values.unsqueeze(1))
 
+    # backpropagation
     optimizer.zero_grad()
     loss.backward()
     for param in policy_net.parameters():
         param.grad.data.clamp_(-1, 1)
+    # update weights
     optimizer.step()
 
 
@@ -94,14 +109,17 @@ def train():
     obs = env.reset()
     state = get_state(obs)
 
+    # times for logging
     train_start = datetime.datetime.now()
     episode_start = train_start
 
     for steps_done in range(STEPS):
 
+        # if we want to render/show the game being played
         if args.render:
             env.render()
 
+        # select an action with decaying epsilon greedy
         action = select_action(state, steps_done)
         
         # take one action and observe the outcome
@@ -109,6 +127,7 @@ def train():
         
         episode_rewards[-1] += reward
 
+        # check is the game has ended
         if not done:
             next_state = get_state(obs)
         else:
@@ -123,28 +142,35 @@ def train():
         state = next_state
 
         if done:
+            # time calculation for logging
             episode_end = datetime.datetime.now()
             episode_time = episode_end - episode_start
             tot_time = episode_end - train_start
             episode_start = episode_end
 
+            # average reward of the last 50 epochs
             avg_50 = np.round(np.mean(episode_rewards[-50:]), 1)
 
+            # logs (print, log file, wandb)
             print(f'Total steps: {steps_done} \t Episodes: {len(episode_rewards)} \t Time: {tot_time} \t Episode Time: {episode_time} \t Reward: {episode_rewards[-1]} \t Reward Avg (Last 50): {avg_50}')
             log.info(f'Total steps: {steps_done} \t Episodes: {len(episode_rewards)} \t Time: {tot_time} \t Episode Time: {episode_time} \t Reward: {episode_rewards[-1]} \t Reward Avg (Last 50): {avg_50}')
             wandb.log({'reward': episode_rewards[-1], 'reward avg (last 50)': avg_50})
 
+            # reset environment for next episode
             obs = env.reset()
             state = get_state(obs)
             episode_rewards.append(0.0)
         
         if steps_done > INITIAL_MEMORY:
             if steps_done % LEARNING_FREQ == 0:
+                # optimize
                 learn()
 
             if steps_done % TARGET_UPDATE == 0:
+                # update target model
                 update_target(policy_net, target_net)
 
+        # save model checkpoint
         if steps_done % SAVE_FREQ == 0:
             time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
             torch.save(policy_net.state_dict(), models_dir + time)
@@ -154,26 +180,36 @@ def train():
 
 
 def test():
+    # time for logging
     time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
     rewards = []
+
     for episode in range(TEST_EP):
+
         obs = env.reset()
         state = get_state(obs)
         total_reward = 0.0
         done = False
+
         while not done:
+            # check if we are trying to attack
             if args.attack:
                 r_tot, loop_i, label, k_i, state = deepfool(state, policy_net, num_actions=num_actions)
+
             action = policy_net(state.to('cuda')).max(1)[1].view(1,1)
 
             if args.render:
                 env.render()
-                t.sleep(0.01)
+                # when attacking, it would be too slow
+                if not args.attack:
+                    t.sleep(0.01)
 
+            # take one action and observe the outcome
             obs, reward, done, info = env.step(action)
 
             total_reward += reward
 
+            # check if the game has ended
             if not done:
                 next_state = get_state(obs)
             else:
@@ -181,18 +217,32 @@ def test():
 
             state = next_state
 
+        # log results of episode
         rewards.append(total_reward)
         print("Finished Episode {} with reward {}".format(episode, total_reward))
         log.info("Finished Episode {} with reward {}".format(episode, total_reward))
 
+    # log results of testing
     avg_reward = np.mean(rewards)
     print("Finished Testing {} Episodes with average reward {}".format(TEST_EP, avg_reward))
     log.info("Finished Testing {} Episodes with average reward {}".format(TEST_EP, avg_reward))
 
+    # close the environment
     env.close()
 
 
 if __name__ == "__main__":
+
+    args = parse_args()
+
+    game = args.game
+
+    models_dir = '../../models/' + game + '/'
+    log_dir = '../../logs/' + game + '/'
+
+    time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+
+    log.basicConfig(filename=log_dir+time+'.log', level=log.DEBUG, format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
 
     # hyperparameters
     BATCH_SIZE = 32
@@ -205,24 +255,27 @@ if __name__ == "__main__":
     LEARNING_FREQ = 4
     INITIAL_MEMORY = 10000
     MEMORY_SIZE = 10 * INITIAL_MEMORY
-    STEPS = int(1e7)
+    STEPS = int(2e7)
     SAVE_FREQ = 10000
     TEST_EP = 2
 
-    args = parse_args()
-    game = 'pong'
-    cap_game = 'Pong'
-
-    models_dir = '../../models/' + game + '/'
-    log_dir = '../../logs/' + game + '/'
-
-    time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    log.info(f'BATCH_SIZE: {BATCH_SIZE}')
+    log.info(f'GAMMA: {GAMMA}')
+    log.info(f'EPS_START: {EPS_START}')
+    log.info(f'EPS_END: {EPS_END}')
+    log.info(f'EPS_DECAY: {EPS_DECAY}')
+    log.info(f'TARGET_UPDATE: {TARGET_UPDATE}')
+    log.info(f'LEARNING_RATE: {BATCH_SIZE}')
+    log.info(f'LEARNING_FREQ: {LEARNING_FREQ}')
+    log.info(f'INITIAL_MEMORY: {INITIAL_MEMORY}')
+    log.info(f'MEMORY_SIZE: {MEMORY_SIZE}')
+    log.info(f'STEPS: {STEPS}')
+    log.info(f'SAVE_FREQ: {SAVE_FREQ}')
+    log.info(f'TEST_EP: {TEST_EP}')
 
     if args.train:
         wandb.init(project='dqn_' + game)
         wandb.run.name = time
-
-    log.basicConfig(filename=log_dir+time+'.log', level=log.DEBUG, format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
 
     # if gpu is to be used
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -230,8 +283,9 @@ if __name__ == "__main__":
     log.info(f'Using device: {device}')
 
     # define the environment
-    env = make_atari(cap_game + 'NoFrameskip-v4')
+    env = make_atari(game.capitalize() + 'NoFrameskip-v4')
     env = wrap_deepmind(env, frame_stack=True, scale=True)
+    env = gym.wrappers.Monitor(env, log_dir + time + '_videos')
     num_actions = env.action_space.n
 
     # agent and target
@@ -265,8 +319,9 @@ if __name__ == "__main__":
         wandb.run.save()
 
     if args.test:
-        good_dir = '../../models/tested_good_models/'
-        model = good_dir + game
+        # load model
+        test_dir = '../../models/for_testing/'
+        model = test_dir + game
         print(f'Loading a model: ' + game)
         log.info(f'Loading a model: {game}')
         policy_net.load_state_dict(torch.load(model))
